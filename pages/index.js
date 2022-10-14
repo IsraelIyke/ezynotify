da99d36f8ad47b0252f27e4d1c7c23885837a925
IsraelIyke <nwangwuisrael@gmail.com>
2022-10-14 10:59:55 +0100

    ok

--- pages/index.js
+++ pages/index.js
@@ -29,12 +29,6 @@ export default function Home() {
   return (
     <>
       <div className="explore-page-hero">
-        {/* <img
-          src="./images/bg1c2.jpg"
-          alt="bg"
-          className="bg"
-          // style={{ height: "20rem", width: "20rem" }}
-        /> */}
         <Nav />
         <motion.div
           initial={{ y: -100, opacity: 0 }}
@@ -46,6 +40,12 @@ export default function Home() {
           className="content"
           style={{ textAlign: "center" }}
         >
+          <img
+            src="./images/bg1c2.jpg"
+            alt="bg"
+            className="bg"
+            // style={{ height: "20rem", width: "20rem" }}
+          />
           <div className="card" style={{ color: "white" }}>
             <div className="card__content">
               <div className="card__front">
@@ -92,9 +92,54 @@ export default function Home() {
           <Link href="/sign-up">
             <a className="back-btn">Sign Me Up</a>
           </Link>
+          <div class="custom-shape-divider-bottom custom-shape">
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1200 120"
+              preserveAspectRatio="none"
+            >
+              <path
+                d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                class="shape-fill"
+              ></path>
+            </svg>
+          </div>
         </motion.div>
-        <Footer />
       </div>
+      <div className="about">
+        <h2>About</h2>
+        <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum." "Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum."
+        </p>
+        <div class="custom-shape-divider-bottom-1665740462">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+              class="shape-fill"
+            ></path>
+          </svg>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 }
