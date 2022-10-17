35a10de5135522dd13083b5666dbb4467e4d3094
IsraelIyke <nwangwuisrael@gmail.com>
2022-10-17 10:25:03 +0100

    v3

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,74 @@ const { motion, AnimatePresence } = require("framer-motion");
 import "../styles/globals.css";
 import "../styles/textfield.css";
 import { useRouter } from "next/router";
-
 import { useState, useEffect } from "react";
-import Link from "next/link";
 import { supabase } from "../client";
-import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const [authenticatedState, setAuthenticatedState] =
+    useState("not-authenticated");
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        handleAuthChange(event, session);
+        if (event === "SIGNED_IN") {
+          setAuthenticatedState("authenticated");
+          router.push("/profile");
+        }
+        if (event === "SIGNED_OUT") {
+          setAuthenticatedState("not-authenticated");
+        }
+      }
+    );
+    checkUser();
+    return () => {
+      authListener.unsubscribe();
+    };
+  }, []);
+  async function checkUser() {
+    const user = await supabase.auth.user();
+    if (user) {
+      setAuthenticatedState("authenticated");
+    }
+  }
+  async function handleAuthChange(event, session) {
+    await fetch("/api/auth", {
+      method: "POST",
+      headers: new Headers({ "Content-Type": "application/json" }),
+      credentials: "same-origin",
+      body: JSON.stringify({ event, session }),
+    });
+  }
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <motion.div
+        key={router.route}
+        initial="initialState"
+        animate="animateState"
+        exit="exitState"
+        transition={{
+          duration: 0.75,
+        }}
+        variants={{
+          initialState: {
+            opacity: 0,
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+          },
+          animateState: {
+            opacity: 1,
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+          },
+          exitState: {
+            clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+          },
+        }}
+        className="base-page-size"
+      >
+        <Component {...pageProps} />
+      </motion.div>
+    </AnimatePresence>
+  );
 }
 
 export default MyApp;
