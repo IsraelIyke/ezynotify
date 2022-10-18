ec3ee6269890134c8175e1158126916ed1c81274
IsraelIyke <nwangwuisrael@gmail.com>
2022-10-18 09:57:31 +0100

    v3

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -4,7 +4,8 @@ import { supabase } from "../../client";
 import { useRouter } from "next/router";
 import SideBar from "../../components/sidebar";
 import DashNav from "../../components/dash-nav";
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Paper } from "@mui/material";
+import Link from "next/link";
 
 export default function Profile() {
   const [profile, setProfile] = useState(null);
@@ -30,23 +31,68 @@ export default function Profile() {
   return (
     <Box flexGrow={1}>
       <Grid container spacing={1}>
-        <DashNav signOut={signOut} />
+        <Grid item xs={12}>
+          <DashNav signOut={signOut} />
+        </Grid>
+
         <Grid item xs={1.5} md={2.5}>
-          <div style={{ marginTop: "4rem" }}>
+          <div style={{ marginTop: "3.5rem" }}>
             {/* <SideBar profile={profile} /> */}
           </div>
         </Grid>
         <Grid item xs={10.5} md={9.5}>
           <div
             style={{
-              marginTop: "4rem",
+              marginTop: "3.5rem",
               backgroundColor: "white",
               height: "100vh",
+              color: "black",
             }}
           >
             {/* <h2>Hello, {profile.email}</h2>
             <p>User ID: {profile.id}</p> */}
-            <p style={{ color: "black" }}>hello</p>
+            {/* <p style={{ color: "black" }}></p> */}
+            <Grid container spacing={1}>
+              <Grid item xs={12} md={9} style={{ padding: "2rem 2rem" }}>
+                <Grid container>
+                  <Grid item xs={12}>
+                    <h1>Dashboard</h1>
+                  </Grid>
+
+                  <Grid item xs={7} md={6}>
+                    <Link href="/">
+                      <div className="dash-tile search">
+                        <p style={{ fontSize: "0.7rem" }}>
+                          dashboard dashboard dashboard dashboard dashboard
+                        </p>
+                      </div>
+                    </Link>
+                  </Grid>
+
+                  <Grid item xs={5} md={4}>
+                    <Link href="/">
+                      <div className="dash-tile shift referral">
+                        dashboard dashboard dashboard dashboard dashboard
+                      </div>
+                    </Link>
+                  </Grid>
+                  <Grid item xs={5} md={4}>
+                    <div className="dash-tile update">
+                      dashboard dashboard dashboard dashboard dashboard
+                    </div>
+                  </Grid>
+                  <Grid item xs={7} md={6}>
+                    <div className="dash-tile profile shift">
+                      dashboard dashboard dashboard dashboard dashboard
+                    </div>
+                  </Grid>
+                </Grid>
+              </Grid>
+
+              <Grid item xs={12} md={3}>
+                plan and upgrade
+              </Grid>
+            </Grid>
           </div>
         </Grid>
       </Grid>
