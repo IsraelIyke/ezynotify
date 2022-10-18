1bbb1fb29cb7419fa33bdb86b17fbaa25ca0527b
IsraelIyke <nwangwuisrael@gmail.com>
2022-10-18 12:09:19 +0100

    v3

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -35,12 +35,12 @@ export default function Profile() {
           <DashNav signOut={signOut} />
         </Grid>
 
-        <Grid item xs={1.5} md={2.5}>
+        <Grid item xs={1.8} md={2.5}>
           <div style={{ marginTop: "3.5rem" }}>
             {/* <SideBar profile={profile} /> */}
           </div>
         </Grid>
-        <Grid item xs={10.5} md={9.5}>
+        <Grid item xs={10.2} md={9.5}>
           <div
             style={{
               marginTop: "3.5rem",
@@ -53,17 +53,29 @@ export default function Profile() {
             <p>User ID: {profile.id}</p> */}
             {/* <p style={{ color: "black" }}></p> */}
             <Grid container spacing={1}>
-              <Grid item xs={12} md={9} style={{ padding: "2rem 2rem" }}>
+              <Grid
+                item
+                xs={12}
+                md={9}
+                style={{ padding: "2rem 1.2rem 2rem 1rem" }}
+                className="dash-options-container"
+              >
                 <Grid container>
                   <Grid item xs={12}>
                     <h1>Dashboard</h1>
                   </Grid>
 
-                  <Grid item xs={6.5} md={6}>
+                  <Grid item xs={6.5} md={7}>
                     <Link href="/">
                       <div className="dash-tile search">
-                        <p style={{ fontSize: "0.7rem" }}>
-                          dashboard dashboard dashboard dashboard dashboard
+                        <h3 className="dash-option-title">
+                          Notification
+                          <hr style={{ color: "white", height: "0.2rem" }} />
+                        </h3>
+                        <p className="dash-option-content">
+                          create search notification
+                          <br />
+                          view search status
                         </p>
                       </div>
                     </Link>
@@ -72,22 +84,38 @@ export default function Profile() {
                   <Grid item xs={5.5} md={4}>
                     <Link href="/">
                       <div className="dash-tile shift referral">
-                        <p style={{ fontSize: "0.8rem" }}>
-                          dashboard dashboard dashboard dashboard dashboard
-                        </p>{" "}
+                        <h3 className="dash-option-title">
+                          Web Update
+                          <hr style={{ color: "white", height: "0.2rem" }} />
+                        </h3>
+                        <p className="dash-option-content">
+                          Get notification for every upload in a website
+                        </p>
                       </div>
                     </Link>
                   </Grid>
                   <Grid item xs={5.5} md={4}>
                     <div className="dash-tile update">
-                      <p style={{ fontSize: "0.9rem" }}>
-                        dashboard dashboard dashboard dashboard dashboard
-                      </p>{" "}
+                      <h3 className="dash-option-title">
+                        Referral
+                        <hr style={{ color: "white", height: "0.2rem" }} />
+                      </h3>
+                      <p className="dash-option-content">
+                        Refer us to your friends and accumulate upgrade points
+                      </p>
                     </div>
                   </Grid>
-                  <Grid item xs={6.5} md={6}>
+                  <Grid item xs={6.5} md={7}>
                     <div className="dash-tile profile shift">
-                      dashboard dashboard dashboard dashboard dashboard
+                      <h3 className="dash-option-title">
+                        Profile
+                        <hr style={{ color: "white", height: "0.2rem" }} />
+                      </h3>
+                      <p className="dash-option-content">
+                        Change password
+                        <br />
+                        Update email, telegram, facebook
+                      </p>
                     </div>
                   </Grid>
                 </Grid>
