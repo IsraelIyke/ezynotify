e238a5dd0989117f27088122ad536c2612a6b987
IsraelIyke <nwangwuisrael@gmail.com>
2022-11-17 09:30:10 +0100

    ok

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,29 @@ const { motion } = require("framer-motion");
 
 export default function Home() {
   return (
-    <motion.div className="explore-page-hero">
+    <motion.div
+      // key={router.route}
+      initial="initialState"
+      animate="animateState"
+      exit="exitState"
+      transition={{
+        duration: 0.75,
+      }}
+      variants={{
+        initialState: {
+          opacity: 0,
+          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+        },
+        animateState: {
+          opacity: 1,
+          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+        },
+        exitState: {
+          clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+        },
+      }}
+      className="explore-page-hero"
+    >
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={0}>
           <Grid item xs={12} md={12}>
