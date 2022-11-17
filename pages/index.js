37d8d476936683f8d13c7df3e73e19a999fe44b5
IsraelIyke <nwangwuisrael@gmail.com>
2022-11-17 09:35:36 +0100

    ok

--- pages/index.js
+++ pages/index.js
@@ -14,59 +14,37 @@ const { motion } = require("framer-motion");
 
 export default function Home() {
   return (
-    <motion.div
-      // key={router.route}
-      initial="initialState"
-      animate="animateState"
-      exit="exitState"
-      transition={{
-        duration: 0.75,
-      }}
-      variants={{
-        initialState: {
-          opacity: 0,
-          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
-        },
-        animateState: {
-          opacity: 1,
-          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
-        },
-        exitState: {
-          clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
-        },
-      }}
-      className="explore-page-hero"
-    >
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={0}>
-          <Grid item xs={12} md={12}>
-            <Nav />
-          </Grid>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={0}>
+        <Grid item xs={12} md={12}>
+          <Nav />
+        </Grid>
+        <motion.div>
           <Grid item xs={12} md={12}>
             <Banner />
           </Grid>
-          <Grid item xs={12} md={6}>
-            <Image />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Content />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Content2 />
-          </Grid>
-
-          <Grid item xs={12} md={6}>
-            <Image2 />
-          </Grid>
-          <Grid item xs={12} md={12}>
-            <Testimonial />
-          </Grid>
-          <Grid item xs={12} md={12}>
-            <Footer />
-          </Grid>
-          <ScrollButton />
+        </motion.div>
+        <Grid item xs={12} md={6}>
+          <Image />
         </Grid>
-      </Box>
-    </motion.div>
+        <Grid item xs={12} md={6}>
+          <Content />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Content2 />
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <Image2 />
+        </Grid>
+        <Grid item xs={12} md={12}>
+          <Testimonial />
+        </Grid>
+        <Grid item xs={12} md={12}>
+          <Footer />
+        </Grid>
+        <ScrollButton />
+      </Grid>
+    </Box>
   );
 }
