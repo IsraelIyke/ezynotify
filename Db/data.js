382f87467a26580c268b8e95003ad6be53fd24b7
IsraelIyke <nwangwuisrael@gmail.com>
2022-12-03 09:01:41 +0100

    ad

--- Db/data.js
+++ Db/data.js
@@ -22,4 +22,16 @@ export const data = [
     answer:
       "A third-party payment processor (also known as a payment provider or payment gateway) lets you accept online payments.",
   },
+  {
+    id: 5,
+    question: "Why are I not receiving email or telegram messages? ",
+    answer:
+      "This can be caused by incorrect email address, telegram(for premium plan) submitted. It can also be caused by a spelling mistake while entering the website or keyword. In some other cases for the email specifically, your mails may be forwarded to your spam",
+  },
+  {
+    id: 6,
+    question: "What is telegram chat id? ",
+    answer:
+      "The chat id is similar to the username. The major differences are that the chat id is used by telegram bots to send messages and also, it a 10 digits number unlike username that can be alphanumeric",
+  },
 ];
